b0edf5dbf5530099f0fbe12ec689f0ef152a714b
DESKTOP-7338OS6\LHQ <LHQ>
2023-04-12 10:28:28 +0800

    fix: 删除button Signed-off-by: DESKTOP-7338OS6\LHQ <LHQ>

--- packages/yto-custom/src/components/button/main.ts
+++
@@ -1,8 +0,0 @@
-import ButonTable from './src/index.vue'
-
-export { ButonTable }
-export default {
-    install: (App:any) => {
-        App.component("yto-button", ButonTable) //注意避坑 -> 组件内部一定要写name属性
-    }
-}
