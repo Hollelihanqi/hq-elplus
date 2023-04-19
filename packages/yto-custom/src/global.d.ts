dbaeae2bed1a3a493956e212d46e02f057869ba4
DESKTOP-7338OS6\LHQ <LHQ>
2023-04-19 16:21:57 +0800

    feat: d.ts 文件提交 Signed-off-by: DESKTOP-7338OS6\LHQ <LHQ>

---
+++ packages/yto-custom/src/global.d.ts
@@ -0,0 +1,26 @@
+import type * as components from './components'
+
+/**
+ * Volar 插件实现代码提示
+ *
+ * https://github.com/johnsoncodehk/volar/blob/master/extensions/vscode-vue-language-features/README.md
+ */
+
+declare module '@vue/runtime-core' {
+  export interface GlobalComponents {
+    YtoProTable: typeof components.YtoProTable
+    YtoTable: typeof components.YtoTable
+    YtoMenu: typeof components.YtoMenu
+    YtoEchart: typeof components.YtoEchart
+  }
+
+  interface ComponentCustomProperties {
+
+  }
+}
+
+declare global {
+  const __DEV__: boolean
+}
+
+export { }
