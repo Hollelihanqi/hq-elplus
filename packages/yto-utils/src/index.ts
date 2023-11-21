a69114ec59bb159e9e671b123cbae5a475edba83
YTO <02625420@yto.net.com>
2023-11-21 19:00:49 +0800

    fix: 添加下载方法

--- packages/yto-utils/src/index.ts
+++ packages/yto-utils/src/index.ts
@@ -3,4 +3,6 @@ import request from "./modules/request";
 export * from "./modules/utils";
 export * from "./bridge/index";
 export * from './modules/numberUtils'
+export * from './download/index'
+
 export { request }
