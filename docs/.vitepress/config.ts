462e2316621d315575e6b8728eebcc5b243a8d4a
weichunpei <02348618@yto.net.cn>
2024-01-24 11:12:56 +0800

    feat: 提供系统维护/升级统一提示页面

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,7 +2,7 @@
  * @Author: DESKTOP-7338OS6\LHQ LHQ
  * @Date: 2023-03-24 17:52:24
  * @LastEditors: weichunpei
- * @LastEditTime: 2024-01-18 14:43:36
+ * @LastEditTime: 2024-01-24 10:49:47
  * @FilePath: \yto-engine\docs\docs\.vitepress\config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AErts
  */
@@ -142,6 +142,16 @@ export default defineConfig({
             {
               text: "其他方法",
               link: "/examples/utils/other.md",
+            },
+          ],
+          collapsed: false,
+        },
+        {
+          text: "其他",
+          items: [
+            {
+              text: "系统维护统一提示",
+              link: "/examples/others/systemMaintenance.md",
             },
           ],
           collapsed: false,
