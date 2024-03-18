34cb36a4bee36678fc448d0537712fd54c0b9f5e
DESKTOP-7338OS6\LHQ <LHQ>
2024-03-18 14:13:24 +0800

    fix: 修复tsconfig配置

--- docs2/examples/button/basic.vue
+++ docs2/examples/button/basic.vue
@@ -37,12 +37,5 @@
 </template>
 
 <script lang="ts" setup>
-import {
-  Check,
-  Delete,
-  Edit,
-  Message,
-  Search,
-  Star,
-} from '@element-plus/icons-vue'
+import { Check, Delete, Edit, Message, Search, Star } from "@element-plus/icons-vue";
 </script>
