2bd5008bfedc3d728062fdfe95e10e7f688bcd65
DESKTOP-7338OS6\LHQ <LHQ>
2024-07-02 17:46:47 +0800

    feat: jsonview 组件优化

--- packages/yto-custom/src/components/json-viewer/src/MyTransition.vue
+++ packages/yto-custom/src/components/json-viewer/src/MyTransition.vue
@@ -2,7 +2,7 @@
  * @Author: DESKTOP-7338OS6\LHQ LHQ
  * @Date: 2024-06-28 13:55:59
  * @LastEditors: DESKTOP-7338OS6\LHQ LHQ
- * @LastEditTime: 2024-06-28 14:50:01
+ * @LastEditTime: 2024-06-28 16:12:22
  * @FilePath: \yto-engine\packages\yto-custom\src\components\json-viewer\src\MyTransition.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -20,7 +20,8 @@
 
 .v-enter-from,
 .v-leave-to {
-  transform: translateX(20px);
+  /* transform: translateX(20px); */
   opacity: 0;
+  height: 0;
 }
 </style>
